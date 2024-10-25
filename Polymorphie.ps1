12f547d400dc154bd20f1fb90bfe2b7a56acecf6
DanielR <113525395+Shimotakii@users.noreply.github.com>
2024-10-25 13:19:07 +0200

    Add files via upload

---
+++ Polymorphie.ps1
@@ -0,0 +1,26 @@
+﻿class Fahrzeug {
+    [void] Fahren() {
+        Write-Host "Das Fahrzeug fährt."
+    }
+}
+
+class Auto : Fahrzeug {
+    [void] Fahren() {
+    Write-Host "Das Auto fährt schnell."
+    }
+}
+
+class Fahrrad : Fahrzeug {
+    [void] Fahren() {
+        Write-Host "Das Fahrrad fährt langsam."
+    }
+}
+
+$meinAuto = [Auto]::new()
+$meinFahrrad = [Fahrrad]::new()
+
+$meinAuto.Fahren()
+$meinFahrrad.Fahren()
+
+$fahrzeug = [Fahrzeug]::new()
+$fahrzeug.Fahren()
---
+++ Vererbung.ps1
@@ -0,0 +1,36 @@
+﻿class Fahrzeug {
+
+    # Properties definieren
+    [string]$Farbe
+    [int]$Geschwindigkeit
+    
+    # Methoden definieren
+    [void] Beschleunigen([int]$wert) {
+        $this.Geschwindigkeit += $wert
+        Write-Host "Das Fahrzeug beschleunigt auf $($this.Geschwindigkeit) km/h."
+    }
+
+    # Konstruktor definieren
+    Fahrzeug([string]$farbe, [int]$geschwindigkeit) {
+        $this.farbe = $farbe
+        $this.Geschwindigkeit = $geschwindigkeit
+    }
+}
+
+class Auto : Fahrzeug {
+
+    [string]$Marke
+
+    Auto([string]$farbe, [int]$geschwindigkeit, [string]$marke) : base($farbe, $geschwindigkeit) {
+        $this.Marke = $marke
+    }
+
+    [void] ZeigeMarke() {
+        Write-Host "Marke des Autos: $($this.Marke)"
+    }
+
+}
+
+$meinAuto = [Auto]::new("Weiß", 100, "BMW")
+$meinAuto.ZeigeMarke()
+$meinAuto.Beschleunigen(20)
